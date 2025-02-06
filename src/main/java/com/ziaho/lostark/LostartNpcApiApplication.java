184c7d85200f4fc9052af3ec63d5e00293abcc25
zziaho <rlawlgh1100@naver.com>
2025-02-06 22:23:17 +0900

    feat: init

---
+++ src/main/java/com/ziaho/lostark/LostartNpcApiApplication.java
@@ -0,0 +1,13 @@
+package com.ziaho.lostark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostartNpcApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LostartNpcApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ziaho/lostark/npcapi/LostartNpcApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ziaho.lostark.npcapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostartNpcApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
